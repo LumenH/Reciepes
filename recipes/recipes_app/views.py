be35fd62dc48a3f4df5cf34578323d6aa8e09001
ArnaudDroxler <arnaud.droxler@gmail.com>
2019-05-04 18:28:57 +0200

    Add first view, probably broken need to be checked with postman

--- recipes/recipes_app/views.py
+++ recipes/recipes_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class HomeView(APIView):
+
+    def get(self, request):
+        return Response({'status' : '200'})
